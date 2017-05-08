287ea473fe3e241ecc2ac329bdaca1acfd1bf5fc
Nabil Freeman <nabil@instafork.com>
2017-05-08 17:08:35 +0100

    bug fixes and error checking

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ anora("freeman") // 'a'
 */
 
 module.exports = function(word){
+	if(!word || typeof word !== "string") throw new Error("Please supply a string as the first (and only) argument.");
+	
+	if(!word.length) throw new Error("You sent an empty string.");
+
+	word = word.toLowerCase();
+
 	var char = word[0];
 
 	if(['a', 'e', 'i', 'o', 'u'].indexOf(char) > -1){
